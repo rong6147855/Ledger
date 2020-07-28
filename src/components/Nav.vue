36e9c49bd024a93b38b33a83acbd0594a194aa2c
yangrong <rong6147855@foxmail.com>
2020-07-28 22:43:34 +0800

    添加字体

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -22,12 +22,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
+
 nav {
   display: flex;
-  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
   flex-direction: row;
   font-size: 12px;
   > .item {
+    color: $color-highlight;
     padding: 2px 0;
     width: 33.33333%;
     display: flex;
