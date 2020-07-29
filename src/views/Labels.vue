3c5f802de33fd7bedec80052971fec0506703441
yangrong <rong6147855@foxmail.com>
2020-07-29 15:24:23 +0800

    标签页初稿

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,13 +1,25 @@
 <template>
   <Layout>
-    <ol>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
+    <ol class="tags">
+      <li>
+        <span>衣</span>
+        <Icon name="right" />
+      </li>
+      <li>
+        <span>食</span>
+        <Icon name="right" />
+      </li>
+      <li>
+        <span>住</span>
+        <Icon name="right" />
+      </li>
+      <li>
+        <span>行</span>
+        <Icon name="right" />
+      </li>
     </ol>
-    <div>
-      <button>新建标签</button>
+    <div class="createTag-wrapper">
+      <button class="createTag">新建标签</button>
     </div>
   </Layout>
 </template>
@@ -18,4 +30,36 @@ import Vue from "vue";
 export default Vue.extend({ name: "Labels" });
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.tags {
+  background: white;
+  font-size: 16px;
+  padding-left: 16px;
+  > li {
+    min-height: 44px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid #e6e6e6;
+    svg {
+      width: 18px;
+      height: 18px;
+      color: #666;
+      margin-right: 16px;
+    }
+  }
+}
+.createTag {
+  background: #767676;
+  color: white;
+  border-radius: 4px;
+  border: none;
+  height: 40px;
+  padding: 0 16px;
+  &-wrapper {
+    text-align: center;
+    padding: 16px;
+    margin-top: 44-16px;
+  }
+}
+</style>
