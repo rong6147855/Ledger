0affa47f789f84737330c476565c7cb2ae0bd974
yangrong <rong6147855@foxmail.com>
2020-07-31 16:52:33 +0800

    自己做统计页面1

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,12 +1,29 @@
 <template>
-  <Layout>Statistics.vue</Layout>
+  <Layout>
+    <Types class-prefix="zzz" :value.sync="yyy" />
+  </Layout>
 </template>
 
-<script lang="ts">
-import Vue from "vue";
-
-export default Vue.extend({ name: "Statistics" });
-</script>
-
-<style scoped>
+<style scoped lang="scss">
+::v-deep .zzz-item {
+  background: white;
+  border: 1px solid red;
+  &.selected {
+    background: rgb(254, 219, 63);
+    &::after {
+      display: none;
+    }
+  }
+}
 </style>
+
+<script lang="ts">
+import Types from "@/components/Money/Types.vue";
+import Vue from "vue";
+// import Component from 'vue-class-component';
+import { Component } from "vue-property-decorator";
+@Component({ components: { Types } })
+export default class Statistics extends Vue {
+  yyy = "-";
+}
+</script>
